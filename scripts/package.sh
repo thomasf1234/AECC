e84c7819e695392ca2b332db44c6a76a469b9d84
Thomas F <thomas.fisher@footballpools.com>
2017-02-27 19:56:34 +0000

    [#0] TF : adding package utility

---
+++ scripts/package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#exit on error
+set -e
+
+echo "Clearing out previously built packages"
+rm -rf build/*.tgz
+
+git_revision=$(git rev-parse HEAD)
+echo "git:${git_revision}" > public/version.txt
+
+echo "Packaging...."
+tar -zc -f build/aecc-full.tgz --exclude-from=scripts/files_to_exclude.txt .
+
+echo "Done"
+
